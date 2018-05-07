602d1fabfe2811574ff3f667c296af7866dccf0f
vibhorsingh11 <vibhorsinghcs@gmail.com>
2018-05-07 10:28:54 +0530

    Some Chnages

--- EmployeeData-Angular/src/app/Employee/employee.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Iemployee = /** @class */ (function () {
-    function Iemployee() {
-    }
-    return Iemployee;
-}());
-exports.Iemployee = Iemployee;
-//# sourceMappingURL=employee.js.map
